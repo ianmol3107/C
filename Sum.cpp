2ffdf43214b649e606cf839d14102dc7a5b9f014
ianmol3107 <107785867+ianmol3107@users.noreply.github.com>
2022-06-23 12:50:46 +0530

    Add files via upload

---
+++ Sum.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{ 
+int a,b,c;
+printf("Enter your First Number: ");
+scanf("%d", &a);
+printf("Enter Your Second Number: ");
+scanf("%d", &b);
+c= a+b;
+printf("The Two numbers Sum is: %d", c);
+return 0;
+
+}
