7d15220c0146748ece36d22aec4174ada3d914f3
Anmol Diwedi <ianmol3107@gmail.com>
2022-06-28 09:49:34 +0530

    Find Area of Circle?

---
+++ Area Of circle.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main()
+{
+	float pi=3.14;
+	float r,cir;
+	printf("Enter the Value of radius: ");
+	scanf("%f", &r);
+	cir= pi*r*r;
+	printf("Area of circle is: %f", cir);
+	
+}
