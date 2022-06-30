2cc14269724ca1a2b0d98d306f4a2598785e686b
Anmol Diwedi <ianmol3107@gmail.com>
2022-06-30 14:55:53 +0530

    Check Tax Paid by a Person!

---
+++ Tax Paid by a Person.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+main()
+{
+	float tax, income;
+	printf("Enter your Income: ");
+	scanf("%f", &income);
+	if ((income>=250000) && income<=500000)
+	{
+		tax = tax + 0.05*(income-250000);
+	}
+	if (income>500000 && income<=1000000)
+	{
+		tax = tax + 0.20*(income-500000);
+	}
+	if (income>=1000000)
+	{
+		tax = tax + 0.30*(income-1000000);
+	}
+	printf("Total tax is: %f", tax);
+}
