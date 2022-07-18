dc8b6784bcc4dad4412359de4dc01fdcc983b38f
Anmol Diwedi <ianmol3107@gmail.com>
2022-07-18 21:05:58 +0530

    Using Arrays!!!

---
+++ Pointer Arthmetic.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main()
+{
+int a;
+int *ptr= &a;
+printf("The Value of ptr is %u\n", ptr);
+ptr++; 
+//ptr = ptr+1;   { We can use any arthmetic like (ptr+1),(ptr+2)(ptr-1),(ptr-2) and (ptr++),(ptr--) also we can use }
+//ptr = ptr +2;
+//ptr = ptr -2;
+printf("The Value of ptr is %u\n", ptr);
+
+}
