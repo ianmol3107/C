d9dd32f7b04d14ea8c2670e4e83e76670a1d0b2a
Anmol Diwedi <ianmol3107@gmail.com>
2022-08-07 09:31:05 +0530

    Using Strings!!!

---
+++ Easy way to print strings.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+main()
+{
+	//char str[]= "ANMOL Diwedi";                                                   //-1 
+	//char *ptr=str;              // Here we can only use *ptr="Anmol Diwedi" too!!!//-2
+	
+	char str[]="ANMOL Diwedi";    // we can use this also and 1 or 2 too...!!!
+	printf("%s", str);           // %s is use for strings!!!
+}
