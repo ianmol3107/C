ca75a62f52344a72b1900c829d89e69fd12c91f0
Anmol Diwedi <ianmol3107@gmail.com>
2022-09-11 13:03:49 +0530

    Add files via upload

---
+++ Multiple task for Multiplication.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void devide()
+{
+	int a,b,devide;
+	printf("ENTER TWO NUMBERS:-");
+	scanf("%d%d",&a,&b);
+	devide=a/b;
+	printf("DEVIDE OF NUMBERS :- %d\n",devide);
+}
+int main()
+{
+	devide();
+	devide();
+	devide();
+	devide();
+	devide();
+	devide();
+	devide();
+	devide();
+	devide();
+	devide();
+	devide();
+	devide();
+    devide();
+	devide();
+	devide();
+}
