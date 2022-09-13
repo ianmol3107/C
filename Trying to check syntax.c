d6c751cfa2ec5aba62043c600eb6a409e239bff5
Anmol Diwedi <ianmol3107@gmail.com>
2022-09-13 13:53:14 +0530

    Create Trying to check syntax.c

---
+++ Trying to check syntax.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int a=2,b=4, c=16;
+    if(a>b)
+        if(a>c)
+            printf("a is greater");
+        else
+        printf("b & c is smaller than a");
+        
+    else
+       
+        
+    return 0;
+}
