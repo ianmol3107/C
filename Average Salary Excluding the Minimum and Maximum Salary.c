462ebdbc9df409314c07b2318b7200b12de13a91
Anmol Diwedi <ianmol3107@gmail.com>
2022-10-04 19:45:08 +0530

    Create Average Salary Excluding the Minimum and Maximum Salary.c

---
+++ Average Salary Excluding the Minimum and Maximum Salary.c
@@ -0,0 +1,15 @@
+
+
+double average(int* salary, int salarySize){
+    int max=salary[0], min=salary[0];
+    double sum=0;
+    for(int i=0; i<salarySize; i++){
+        if(salary[i]<min){
+            min=salary[i];
+        }
+        if(salary[i]>max){
+            max=salary[i];
+        }
+        sum= sum+salary[i];
+    }return (sum-max-min)/(salarySize-2);
+}
