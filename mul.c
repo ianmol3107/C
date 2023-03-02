580bf503c21084a42800978be6e472b7c72d4c9c
Anmol Diwedi <ianmol3107@gmail.com>
2023-03-02 19:19:17 +0530

    Add files via upload

---
+++ mul.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int a,b,c,d,i,j,k;
+    printf("Enter row of ist matrix:- ");
+    scanf("%d",&a);
+    printf("Enter col of ist matrix:- ");
+    scanf("%d",&b);
+    printf("Enter row of ist matrix:- ");
+    scanf("%d",&c);
+    printf("Enter col of ist matrix:- ");
+    scanf("%d",&d);
+
+    if(b!=)
+
+
+
+}
