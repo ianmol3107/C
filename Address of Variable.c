cb56f0319a0b903ac2bab9bd77f128d64a39636c
Anmol Diwedi <ianmol3107@gmail.com>
2023-03-05 16:09:41 +0530

    Update Address of Variable.c

--- Address of Variable.c
+++ Address of Variable.c
@@ -8,6 +8,7 @@ printf("The Address of a is= %u\n", &a);
 printf("The Address of a is= %u\n", ptr);
 
 printf("\n\n");
+  
 
 printf("The value of a is= %d\n", *ptr);
 printf("The value of a is= %d\n", a);
